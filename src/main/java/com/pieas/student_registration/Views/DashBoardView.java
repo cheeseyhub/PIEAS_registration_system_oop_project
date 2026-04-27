878714c11007e7288620394907a17022166135fe
CheeseyHub <muhammedibrahim613@gmail.com>
2026-04-27 06:25:23 +0500

    Made the dashboard route and also learned how to do custom styling

---
+++ src/main/java/com/pieas/student_registration/Views/DashBoardView.java
@@ -0,0 +1,12 @@
+package com.pieas.student_registration.Views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("dashboard")
+public class DashBoardView extends VerticalLayout {
+    public DashBoardView() {
+
+    }
+
+}
