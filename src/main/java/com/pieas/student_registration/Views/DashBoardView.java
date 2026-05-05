a78d7291fbf6abe126dbd7993131beb47da33041
Zohaib Kaleem <223814646+zohaib-kaleem@users.noreply.github.com>
2026-05-06 03:07:21 +0500

    Change the dashboard Design

--- src/main/java/com/pieas/student_registration/Views/DashBoardView.java
+++ src/main/java/com/pieas/student_registration/Views/DashBoardView.java
@@ -1,38 +1,55 @@
 package com.pieas.student_registration.Views;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("dashboard")
+@Route("main")
 
-public class DashboardView e
+public class DashboardView extends VerticalLayout {
+    public DashboardView() {
+        add(
+                new Main());
+    }
+}
 
-        
- 
+class Header extends HorizontalLayout {
+    public Header() {
 
- 
+    }
 
- 
+}
 
-    
-class DashboardContent extends Horizo
+class Main extends HorizontalLayout {
+    public Main() {
+        add(
+                new Sidebar());
+    }
+}
 
-    talLayout {
+class Sidebar extends VerticalLayout {
+    public Sidebar() {
+        String buttons[][] = {
+                { "Student Registration", "registration" },
+                { "Course Enroll", "courses" },
+                { "View Results", "result" },
+                { "Change Password", "changePassword" },
+                { "Log Out", "logout" }
+        };
 
-    private SidebarMenu sidebar = new SidebarM enu();
-    private MainContentArea mainContent = new MainContentArea();
+        for (String btn[] : buttons) {
+            addButton(btn[0], btn[1]);
+        }
+    }
 
-    public DashboardContent() {
-        add(sidebar, mainContent);
-        setWidthFull();
-        expand(mainContent);
+    private void addButton(String text, String destination) {
+        Button button = new Button(text);
+        button.addClickListener(e -> {
+            UI.getCurrent().navigate(destination);
+        });
 
-        // Navigation logic stays with the content
-     
- 
-
-setJustifyConte
-
+        add(button);
     }
 }
