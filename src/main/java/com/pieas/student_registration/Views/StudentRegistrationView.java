4129278365e7a818ec5cc6c82bef5968629c9d78
Zohaib Kaleem <223814646+zohaib-kaleem@users.noreply.github.com>
2026-05-09 21:53:35 +0500

    Making the Registration Page

--- src/main/java/com/pieas/student_registration/Views/StudentRegistrationView.java
+++ src/main/java/com/pieas/student_registration/Views/StudentRegistrationView.java
@@ -1,8 +1,9 @@
 package com.pieas.student_registration.Views;
 
-import java.text.Normalizer.Form;
-
 import com.pieas.student_registration.Views.TemplateClasses.*;
+import com.vaadin.copilot.shaded.helger.base.email.EmailAddress;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
@@ -12,9 +13,11 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.frontendtools.installer.VerificationException;
 
 @Route("registration")
 @StyleSheet("styles/style.css")
@@ -23,6 +26,7 @@ import com.vaadin.frontendtools.installer.VerificationException;
 public class StudentRegistrationView extends HorizontalLayout {
     StudentRegistrationView() {
         this.setWidthFull();
+        this.setHeightFull();
         this.setSpacing(false);
         this.setPadding(false);
 
@@ -42,6 +46,8 @@ public class StudentRegistrationView extends HorizontalLayout {
         private VerticalLayout Main() {
             VerticalLayout tempLayoutContainer = new VerticalLayout();
             tempLayoutContainer.addClassName("registration-main");
+            tempLayoutContainer.setWidthFull();
+            tempLayoutContainer.setHeightFull();
 
             tempLayoutContainer.add(Header(), UserInformationForm());
 
@@ -50,6 +56,8 @@ public class StudentRegistrationView extends HorizontalLayout {
 
         private HorizontalLayout Header() {
             HorizontalLayout tempHorizontalLayout = new HorizontalLayout();
+            tempHorizontalLayout.setWidthFull();
+            tempHorizontalLayout.setWidthFull();
 
             Icon icon = new Icon(VaadinIcon.USER_CARD);
             Span iconWrapper = new Span();
@@ -66,7 +74,31 @@ public class StudentRegistrationView extends HorizontalLayout {
 
         private FormLayout UserInformationForm() {
             FormLayout formLayoutContainer = new FormLayout();
+            formLayoutContainer.addClassName("registration-form");
+            formLayoutContainer.setWidthFull();
+            formLayoutContainer.setWidthFull();
 
+            TextField name = new TextField("Name");
+            TextField fatherName = new TextField("Father Name");
+            EmailField emailAddress = new EmailField("Email");
+            TextField rollNo = new TextField("Roll No.");
+
+            NumberField regNo = new NumberField("Registration Number");
+            NumberField cnic = new NumberField("CNIC");
+            TextField Address = new TextField("Registration Number");
+            TextField Domicile = new TextField("Domicile");
+            NumberField phoneNumber = new NumberField("Phone Number");
+            NumberField emergencyPhoneNumber = new NumberField("Emergency Contact Number");
+
+            DatePicker dateOfBirth = new DatePicker("Date Of Birth");
+
+            ComboBox<> department = new ComboBox<>(
+
+            );
+
+            formLayoutContainer.add(
+                    name, fatherName, emailAddress, rollNo, regNo, dateOfBirth, cnic, Address, Domicile, phoneNumber,
+                    emergencyPhoneNumber);
             return formLayoutContainer;
         }
     }
